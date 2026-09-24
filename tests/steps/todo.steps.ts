fe54d6852c919697a753ee8487e28a164d829a89
Your Name <you@example.com>
2026-09-24 12:59:28 +0100

    feat: update todo feature scenarios and add new test results

--- tests/steps/todo.steps.ts
+++ tests/steps/todo.steps.ts
@@ -1,4 +1,3 @@
-import { expect } from '@playwright/test';
 import { createBdd } from 'playwright-bdd';
 import { TodoPage } from '../pages/TodoPage';
 
@@ -6,25 +5,47 @@ const { Given, When, Then } = createBdd();
 
 Given('I am on the home page', async ({ page }) => {
   const todoPage = new TodoPage(page);
+
   await todoPage.gotoHome();
+});
+
+Given('I am on the todo page', async ({ page }) => {
+  const todoPage = new TodoPage(page);
+
+  await todoPage.gotoHome();
+  await todoPage.openTodoPage();
 });
 
 When('I navigate to the todo page', async ({ page }) => {
   const todoPage = new TodoPage(page);
+
+  await todoPage.gotoHome();
   await todoPage.openTodoPage();
 });
 
-When('I add a todo item {string} with type {string}', async ({ page }, taskText: string, todoType: string) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.addTodo(taskText, todoType);
-});
+When(
+  'I add a todo item {string} with type {string}',
+  async ({ page }, taskText: string, todoType: string) => {
+    const todoPage = new TodoPage(page);
 
-Then('I should see {string} in the todo list', async ({ page }, taskText: string) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.expectTodoVisible(taskText);
-});
+    await todoPage.addTodo(taskText, todoType);
+  }
+);
 
-Then('the todo item {string} should include type {string}', async ({ page }, taskText: string, todoType: string) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.expectTodoContains(taskText, todoType);
-});
+Then(
+  'I should see {string} in the todo list',
+  async ({ page }, taskText: string) => {
+    const todoPage = new TodoPage(page);
+
+    await todoPage.expectTodoVisible(taskText);
+  }
+);
+
+Then(
+  'the todo item {string} should include type {string}',
+  async ({ page }, taskText: string, todoType: string) => {
+    const todoPage = new TodoPage(page);
+
+    await todoPage.expectTodoContains(taskText, todoType);
+  }
+);
